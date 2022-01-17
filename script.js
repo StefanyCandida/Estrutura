8cbf9041e590cf58bda5ca0f5dcd96367c20ea8e
StefanyCandida <stefany.candida@gmail.com>
2022-01-16 21:42:42 -0300

    inserindo atividade

---
+++ script.js
@@ -0,0 +1,12 @@
+
+   let name = "Jaiminho"
+   let number = 33
+   let anoAtual = 2022
+   let ano = anoAtual - number
+    console.log(number + ano )
+   console.log("Estou mexendo no console.log")
+    let quartaFeira = true
+    console.log(quartaFeira)
+
+
+    
